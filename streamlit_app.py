aee6bf1a77c4825504759621f915a4cde89884f5
funkonaut <crcorrell@gmail.com>
2021-03-11 15:37:15 -0600

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,12 @@ from vaccinetracker import *
 if __name__ == "__main__":
     # The coordinates for the Texas Capitol.
     st.markdown("# To run the vaccine alert system: \n- Enter in the Lat and Lon coordinates (you can find these using google maps the longitude is negative if it is a W coordiante)\n- Enter in the search radius if you would like to search larger then 20km \n- Once the data is correctly input the program will take over and will launch a web page for a signup appointment when it finds a vaccine\n- To do another search with a differnet location or search radius you will need to refresh the page")
-    old_lat = 0.0
-    old_lon = 0.0
     dist = float(st.slider("Search Radius:",1,100,20))
     home_lat = float(st.number_input("Latitude",step=.00001)) 
     home_lon = float(st.number_input("Longitude",step=.00001))
-    if home_lat != old_lat and home_lon != old_lon: 
+    if home_lat != 0.0 and home_lon != 0.0: 
         home_coords = [home_lat,home_lon]
+        st.map(pd.DataFrame(home_coords,columns=["lat","lon"]))
         app = Application(
             notifiers=[
 #                WinBeeper(200, 400),  # play a 200Hz tone for 400ms
@@ -26,8 +25,6 @@ if __name__ == "__main__":
         )
         start = st.button("Start")
         if start: app.run()
-#        old_lat = home_lat
-#        old_lon = home_lon
     else:
         st.text(f"Please input correct information")
 
