f1761470af7038a067ca7f3d8f128cb70c39f5f5
funkonaut <crcorrell@gmail.com>
2021-03-11 17:23:39 -0600

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,17 @@ from vaccinetracker import *
 
 if __name__ == "__main__":
     # The coordinates for the Texas Capitol.
-    st.markdown("# To run the vaccine alert system: \n- Enter in the Lat and Lon coordinates (you can find these using google maps the longitude is negative if it is a W coordiante)\n- Enter in the search radius if you would like to search larger then 20km \n- Once the data is correctly input the program will take over and will launch a web page for a signup appointment when it finds a vaccine\n- To do another search with a differnet location or search radius you will need to refresh the page")
+    st.markdown("# To run the vaccine alert system: \n- Enter in the Lat and Lon coordinates (you can find these using google maps the longitude is negative if it is a W coordiante)\n- Enter in the search radius if you would like to search larger then 20km \n- Once the data is correctly input the program will display a map of all the relevant HEBs.\n- Click start to begin the alert system links will appear at the bottom of the page as vaccines become availble\n- Once started to do another search with a differnet location or search radius you will need to refresh the page")
     dist = float(st.slider("Search Radius:",1,100,20))
-    home_lat = float(st.number_input("Latitude",step=.00001)) 
-    home_lon = float(st.number_input("Longitude",step=.00001))
+    try:
+        home_lat = float(st.text_input("Latitude")) 
+        home_lon = float(st.text_input("Longitude"))
+    except:
+        st.text("Please make sure your coordinates are entered correctly")
+
     if home_lat != 0.0 and home_lon != 0.0: 
         home_coords = [home_lat,home_lon]
         app = Application(
-            notifiers=[
-                WinBeeper(200, 400),  # play a 200Hz tone for 400ms
-                ConsolePrinter(home_coords),  # Print appointment info to the console
-                LinkOpener()  # Open the link to the signup form
-            ],
             origin=home_coords,
             min_qty=1,  # Don't show notifications less than 1
             max_dist=dist,  # Search within 20 miles
